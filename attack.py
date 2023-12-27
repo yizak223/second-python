5184599ec3c77669cb9ed605ff6216926ea01c06
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-12-27 18:37:56 +0200

    class and objects

--- attack.py
+++ attack.py
@@ -1,9 +1,24 @@
 import random
 
+class Enemy():
+    atkl = 60
+    atkh = 80
+
+    def getAtk(self):
+        print(self.atkl)
+
+enemy1 = Enemy()
+enemy1.getAtk()
+
+enemy2 = Enemy()
+enemy2.getAtk()
+
+'''
 
 playerhp=260
 enemyatkl=60
 enemyatkh=80
+
 
 
 while playerhp > 0:
@@ -19,5 +34,5 @@ while playerhp > 0:
         continue
 
     print("you have low health. You've been teleported to the nearst inn.")
-    break
+    break'''
 
