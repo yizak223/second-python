1b6142936b9dbe1dff8b470c081b18bee2695f71
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-12-28 23:07:42 +0200

    more dynamic

--- attack.py
+++ attack.py
@@ -1,16 +1,18 @@
 import random
 
 class Enemy():
-    atkl = 60
-    atkh = 80
+    def __init__(self, atkl, atkh):
+        self.atkl = atkl
+        self.atkh = atkh
+
 
     def getAtk(self):
         print(self.atkl)
 
-enemy1 = Enemy()
+enemy1 = Enemy(40, 49)
 enemy1.getAtk()
 
-enemy2 = Enemy()
+enemy2 = Enemy(75, 90)
 enemy2.getAtk()
 
 '''
