11120d2b8c3d754eab1bfbc1fbd09bbb7b91c7b6
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-12-28 23:50:22 +0200

    modules classes

--- attack.py
+++ attack.py
@@ -1,19 +1,25 @@
 import random
 
 class Enemy():
+    hp = 200; '''static variable'''
     def __init__(self, atkl, atkh):
-        self.atkl = atkl
+        self.atkl = atkl; '''dynamic variable'''
         self.atkh = atkh
 
 
     def getAtk(self):
-        print(self.atkl)
+        print('atk is:', self.atkl)
+
+    def getHp(self):
+        print('Hp is:', self.hp)
 
 enemy1 = Enemy(40, 49)
 enemy1.getAtk()
+enemy1.getHp()
 
 enemy2 = Enemy(75, 90)
 enemy2.getAtk()
+enemy2.getHp()
 
 '''
 
